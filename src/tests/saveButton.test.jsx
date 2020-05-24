cf22de58aa4a2a8e9baa1e8ceb720f3821a4a83b
nypacheco <nathalia.yp19@gmail.com>
2020-05-24 19:52:17 -0300

    test: create tests to Like and Save components

---
+++ src/tests/saveButton.test.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import SaveButton from '../components/Feed/SaveButton';
+
+describe('SaveButton', () => {
+  it('should change icon when it is clicked', () => {
+    const { getByRole } = render(<SaveButton />);
+
+    const icon = getByRole('img', { hidden: true });
+    expect(icon).toHaveAttribute('data-prefix', 'far');
+
+    fireEvent.click(icon);
+    expect(icon).toHaveAttribute('data-prefix', 'fas');
+  });
+
+  it('should render with correct icon when liked prop is passed', () => {
+    const { getByRole } = render(<SaveButton saved />);
+
+    const icon = getByRole('img', { hidden: true });
+    expect(icon).toHaveAttribute('data-prefix', 'fas');
+  });
+
+  it('should call handleOnClick prop when icon is clicked', () => {
+    const handleOnClick = jest.fn();
+    const { getByRole } = render(<SaveButton handleOnClick={handleOnClick} />);
+
+    const icon = getByRole('img', { hidden: true });
+    fireEvent.click(icon);
+
+    expect(handleOnClick).toHaveBeenCalled();
+  });
+});
