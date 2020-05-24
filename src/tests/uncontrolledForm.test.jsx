9da9960fb041e62d655adccc198d8d8a74c2924d
nypacheco <nathalia.yp19@gmail.com>
2020-05-24 01:54:46 -0300

    test: create uncontrolledform tests

---
+++ src/tests/uncontrolledForm.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import { UncontrolledForm } from '../components/Form';
+
+jest.spyOn(window, 'alert').mockImplementation(() => { });
+
+describe('UncontrolledForm', () => {
+  const value = 'Yuki';
+
+  it('should update input value when onchange is called', () => {
+    const { getByLabelText, getByDisplayValue } = render(<UncontrolledForm />);
+
+    const input = getByLabelText(/nome/i);
+    fireEvent.change(input, { target: { value } });
+
+    expect(getByDisplayValue(value)).toBeInTheDocument();
+  });
+
+  it('should call alert when submit button is clicked', () => {
+    const { getByText, getByLabelText } = render(<UncontrolledForm />);
+
+    const input = getByLabelText(/nome/i);
+    fireEvent.change(input, { target: { value } });
+    fireEvent.click(getByText('Submit'));
+
+    expect(window.alert)
+      .toHaveBeenCalledWith(`Componentes não controlados: { name: ${value} }`);
+  });
+});
