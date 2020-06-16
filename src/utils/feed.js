d42644c33b3192154c82a04380d6341777d2709d
nypacheco <nathalia.yp19@gmail.com>
2020-06-16 19:35:18 -0300

    refactor: remove json server

--- src/utils/feed.js
+++ src/utils/feed.js
@@ -1,3 +1,3 @@
-export const getFeed = () => fetch('http://localhost:3001/posts')
+export const getFeed = () => fetch(`${process.env.REACT_APP_API}/posts`)
   .then((response) => response.json())
   .then((data) => data);
--- src/utils/stories.js
+++ src/utils/stories.js
@@ -1,3 +1,3 @@
-export const getStories = () => fetch('http://localhost:3001/stories')
+export const getStories = () => fetch(`${process.env.REACT_APP_API}/stories`)
   .then((response) => response.json())
   .then((data) => data);
