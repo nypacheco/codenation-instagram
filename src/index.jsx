25a1820fc4ccddf509037d8ab144e3076d8b6128
nypacheco <nathalia.yp19@gmail.com>
2020-06-19 00:02:11 -0300

    refactor: back original sw import

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import * as serviceWorker from './serviceWorker';
+
 import App from './App';
 
 import rootReducer from './reducers';
@@ -29,3 +31,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
